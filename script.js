2e121820466043bdbf5d306c89114d7b41774e1d
Chinurag <129602851+Chinurag@users.noreply.github.com>
2023-09-17 00:41:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// You can add JavaScript functionality here if needed
+const tpbtn2 = document.querySelector(".tpbtn2");
+
+tpbtn2.addEventListener("click", function() {
+    alert("This feature sends a notification to the bus driver, prompting them to stop the BUS!      The button enabling this function becomes active when you are within 1 kilometer of your destination.");
+});
